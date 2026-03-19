7292b542af7dc442098680eea5a963037bfa28e1
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-19 18:48:44 +0100

    Group B

---
+++ Group B/src/App.tsx
@@ -0,0 +1,17 @@
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import TokenRegistry from './components/TokenRegistry';
+
+
+export default function App() {
+
+  return (
+    <>
+          <Navbar />
+          
+          <TokenRegistry/>
+          <Footer/>
+    </>
+  );
+};
