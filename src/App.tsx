0b2141c14c97276380985486de2cdfacba907b01
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-19 17:27:38 +0100

    fix: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import { useState } from 'react'
-import SplashPage from './components/SplashPage'
-import ERC20Page from './components/ERC20page';
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import TokenRegistry from './components/TokenRegistry';
 
-type Page = 'splash' | 'main' | 'tokenRegistry';
 
 export default function App() {
-  const [page, setPage] = useState<Page>('splash')
 
   return (
     <>
-      {page === 'splash' && (
-        <SplashPage onComplete={() => setPage('main')} />
-      )}
-
-      {page === 'main' && (
-        <>
           <Navbar />
-          <ERC20Page />
           
           <TokenRegistry/>
           <Footer/>
-        </>
-       )
-      }
     </>
   );
 };
